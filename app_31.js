9686610f130aedd28134b19c940a596157dc40c4
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-09-26 17:12:52 +0100

    Update app_31.js

--- app_31.js
+++ app_31.js
@@ -1,5 +1,18 @@
 // app_31
 
+// event for button SignIn
+const btnSignIn = document.querySelector('body #container #areaForm .wrapperForm .formLogin  div > input.btnSignIn');
+btnSignIn.addEventListener('click',function(e){
+    e.preventDefault();
+});
+
+// event for button SignUp
+const btnSignUp = document.querySelector('body #container #areaForm .wrapperForm .formSignUp div input.btnSignUp');
+btnSignUp.addEventListener('click',function(e){
+    e.preventDefault();
+});
+
+// event for status Text SignUp
 const statusTextSignUp = document.querySelector('body #container #areaForm .wrapperForm > h5 .statusText.signUp');
 statusTextSignUp.addEventListener('click',function(e){
     e.preventDefault();
@@ -9,7 +22,7 @@ statusTextSignUp.addEventListener('click',function(e){
     wrapperFormSignUp.classList.remove('active');
 });
 
-
+// event for status Text SignIn
 const statusTextSignIn = document.querySelector('body #container #areaForm .wrapperForm > h5 .statusText.signIn');
 statusTextSignIn.addEventListener('click',function(e){
     e.preventDefault();
